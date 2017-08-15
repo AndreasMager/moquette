14b6d7a0290ac44c2f98a19e0f563190a27ec417
Andreas Mager <andreas@mager.eu>
2017-08-15 12:02:47 +0200

    broker/HazelcastInterceptHandler: backwards compatibility ensured

--- broker/src/main/java/io/moquette/interception/HazelcastInterceptHandler.java
+++ broker/src/main/java/io/moquette/interception/HazelcastInterceptHandler.java
@@ -17,41 +17,24 @@
 package io.moquette.interception;
 
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.core.ITopic;
 import io.moquette.interception.messages.InterceptPublishMessage;
 import io.moquette.server.Server;
-import io.netty.buffer.ByteBuf;
-import io.reactivex.schedulers.Schedulers;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import static io.moquette.spi.impl.Utils.readBytesAndRewind;
 
-public class HazelcastInterceptHandler {
+@Deprecated
+public class HazelcastInterceptHandler extends AbstractInterceptHandler {
 
-    private static final Logger LOG = LoggerFactory.getLogger(HazelcastInterceptHandler.class);
     private final HazelcastInstance hz;
 
     public HazelcastInterceptHandler(Server server) {
         this.hz = server.getHazelcastInstance();
-
-        server.getProcessor().getBus().getEvents()
-            .filter(msg -> msg instanceof InterceptPublishMessage)
-            .cast(InterceptPublishMessage.class)
-            .observeOn(Schedulers.single()) // Don't pause netty eventloop thread
-            .subscribe(msg ->  {
-                // TODO ugly, too much array copy
-                ByteBuf payload = msg.getPayload();
-                byte[] payloadContent = readBytesAndRewind(payload);
-
-                LOG.info("{} publish on {} message: {}", msg.getClientID(), msg.getTopicName(),
-                        new String(payloadContent));
-                ITopic<HazelcastMsg> topic = hz.getTopic("moquette");
-                HazelcastMsg hazelcastMsg = new HazelcastMsg(msg);
-                topic.publish(hazelcastMsg);
-            });
     }
 
+    @Override
     public String getID() {
         return HazelcastInterceptHandler.class.getName() + "@" + hz.getName();
+    }
+
+    public void onPublish(InterceptPublishMessage msg) {
+        HazelcastRXHandler.onPublish(hz, msg);
     }
 }
